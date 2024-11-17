2eb94e369da548206adda28b10b0bf42010f92c6
Imma-Nuel-1 <adesanyaolumuyiwa12@gmail.com>
2024-11-17 22:45:57 +0100

    update

--- model/postModel.js
+++ model/postModel.js
@@ -1,3 +1,32 @@
+// const mongoose = require("mongoose");
+
+// const { Schema } = mongoose;
+
+// const PostSchema = new Schema(
+//   {
+//     username: {
+//       type: String,
+//       required: true,
+//     },
+//     postDescription: {
+//       type: String,
+//       required: true,
+//     },
+//     postImage: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Post = mongoose.model("Post", PostSchema);
+// module.exports = Post;
+
+
+
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
@@ -16,9 +45,14 @@ const PostSchema = new Schema(
       type: String,
       required: true,
     },
+    email: {
+      // New field added for associating posts with an email
+      type: String,
+      required: true,
+    },
   },
   {
-    timestamps: true, // Automatically add createdAt and updatedAt timestamps
+    timestamps: true,
   }
 );
 
